10c0951007d6142af86b43578f4aec1677884707
chewbaccar2d2 <fabiom@mesquita.com.br>
2023-04-28 20:29:56 -0300

    Ajuste de cores

--- app/src/main/java/com/example/mesqbeerapp/activity/MainActivity.java
+++ app/src/main/java/com/example/mesqbeerapp/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mesqbeerapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.mesqbeerapp.R;
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, CadastrarProdutoActivity.class);
+        startActivity(intent);
     }
 }
