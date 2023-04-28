bc4649ad38df0354d94a8dff079e20c9159e350b
XoFlango <bruno1999.carlos@hotmail.com>
2023-04-28 20:03:21 -0300

    commit teste

--- app/src/main/java/com/example/mesqbeerapp/model/Produto.java
+++ app/src/main/java/com/example/mesqbeerapp/model/Produto.java
@@ -1,4 +1,13 @@
 package com.example.mesqbeerapp.model;
 
 public class Produto {
+
+    public static boolean comitou;
+    public static int count_commit = 0;
+       if(comitou)
+    {
+        comitou = true;
+        count_commit++;
+    }
+
 }
