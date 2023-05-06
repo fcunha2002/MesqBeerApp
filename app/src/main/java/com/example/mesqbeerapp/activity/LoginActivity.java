f5a3c157a98cdb2d957b6ffc3996c50c4b4d8165
chewbaccar2d2 <fabiom@mesquita.com.br>
2023-05-05 21:15:20 -0300

    Atualização click

--- app/src/main/java/com/example/mesqbeerapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/mesqbeerapp/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.mesqbeerapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.mesqbeerapp.R;
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void entrar(View view) {
+        Intent i = new Intent(this, MenuActivity.class);
+        startActivity(i);
     }
 }
