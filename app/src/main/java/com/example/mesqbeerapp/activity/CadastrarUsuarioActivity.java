092a4fa54c83fce5a661364052a669e6f91b98c3
chewbaccar2d2 <fabiom@mesquita.com.br>
2023-05-25 20:21:13 -0300

    Implementação Cadastro Usuário

--- app/src/main/java/com/example/mesqbeerapp/activity/CadastrarUsuarioActivity.java
+++ app/src/main/java/com/example/mesqbeerapp/activity/CadastrarUsuarioActivity.java
@@ -1,5 +1,6 @@
 package com.example.mesqbeerapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,11 +10,21 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mesqbeerapp.R;
+import com.example.mesqbeerapp.model.Usuario;
+import com.example.mesqbeerapp.util.ConfiguracaoFirebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastrarUsuarioActivity extends AppCompatActivity {
     private EditText campoNome;
     private EditText campoEmail;
     private EditText campoSenha;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +45,11 @@ public class CadastrarUsuarioActivity extends AppCompatActivity {
         if (!textoNome.isEmpty()){
             if (!textoEmail.isEmpty()){
                 if (!textoSenha.isEmpty()){
-//                    Usuario usuario = new Usuario();
-//                    usuario.setNome(textoNome);
-//                    usuario.setEmail(textoEmail);
-//                    usuario.setSenha(textoSenha);
-//                    cadastrarUsuario(usuario);
+                    Usuario usuario = new Usuario();
+                    usuario.setNome(textoNome);
+                    usuario.setEmail(textoEmail);
+                    usuario.setSenha(textoSenha);
+                    cadastrarUsuario(usuario);
                 } else {
                     Toast.makeText(CadastrarUsuarioActivity.this, "Preencha a Senha", Toast.LENGTH_SHORT).show();
                 }
@@ -53,4 +64,47 @@ public class CadastrarUsuarioActivity extends AppCompatActivity {
 //        Intent i = new Intent(this, LoginActivity.class);
 //        startActivity(i);
     }
+
+    public void cadastrarUsuario(final Usuario u){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(u.getEmail(), u.getSenha())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            try {
+                                //Após cadastrar o usuário na área de autenticação,
+                                //grava o restante dos dados no banco de dados
+                                u.setId(autenticacao.getUid());
+                                u.salvar();
+
+                                Toast.makeText(CadastrarUsuarioActivity.this,
+                                        "Usuário cadastrado com sucesso!",
+                                        Toast.LENGTH_SHORT).show();
+                                finish();
+                            } catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        } else {
+                            String excecao;
+                            try {
+                                throw task.getException();
+                            } catch (FirebaseAuthWeakPasswordException e){
+                                excecao = "Digite uma senha mais forte!";
+                            } catch (FirebaseAuthInvalidCredentialsException e){
+                                excecao = "Digite um email válido!";
+                            } catch (FirebaseAuthUserCollisionException e){
+                                excecao = "Esta conta já foi cadastrada!";
+                            } catch (Exception e) {
+                                excecao = "Erro ao cadastrar usuário: " + e.getMessage();
+                                e.printStackTrace();
+                            }
+                            Toast.makeText(CadastrarUsuarioActivity.this,
+                                    excecao,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
---
+++ app/src/main/java/com/example/mesqbeerapp/model/Usuario.java
@@ -0,0 +1,68 @@
+package com.example.mesqbeerapp.model;
+
+import com.example.mesqbeerapp.util.ConfiguracaoFirebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+    private String id;
+    private String nome;
+    private String email;
+    private String senha;
+    private String endereco;
+
+    public Usuario() {
+    }
+
+    public void salvar(){
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(getId());
+
+        usuarioRef.setValue(this);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Exclude //Esta anotação faz com que o campo não seja gravado no
+             //banco de dados do firebase
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
