664cde55c36abc4fe4ca63d97df99448f3f550d0
chewbaccar2d2 <fabiom@mesquita.com.br>
2023-06-02 20:26:28 -0300

    Implemantação SplashScreenActivity

---
+++ app/src/main/java/com/example/mesqbeerapp/activity/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.mesqbeerapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mesqbeerapp.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 5000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
